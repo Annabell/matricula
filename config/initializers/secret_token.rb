ae78f69aea0a34f89b6be6e6b8b331fa1e374a5b
henrique <henrique@henrique-N450.(none)>
2013-04-08 17:15:34 -0300

    open house

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Matricula::Application.config.secret_token = 'f40ff40a0827d8f681549761fdf5c92cbc7681c4e841e07e8eb522ba4155f00dd8372d583f2a41dff61a3098c181f141ffaa0f4bfe3ce3d0e5f85d087c735622'
