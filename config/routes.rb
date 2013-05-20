1632101dd58fc11be3394131c2fe8808a358d530
Pedro Henrique Passalini <henrique.passalini@gmail.com>
2013-05-20 17:57:28 -0300

    Adicionada index para o site e correção no spec de login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Matricula::Application.routes.draw do
+  get "site/index"
+
   devise_for :users
 
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
+  root :to => "site#index"
 end
--- spec/acceptance/login_spec.rb
+++ spec/acceptance/login_spec.rb
@@ -4,21 +4,22 @@ require 'spec_helper'
 feature 'login and register' do
   
   scenario 'a guest user trying to register yourself' do
-    visit '/admin'
+    visit rails_admin_path
     expect(page).to have_content 'Você precisa registrar-se ou fazer login para continuar.'
 
     expect { visit '/users/sign_up' }.to raise_error ActionController::RoutingError
   end
 
-  scenario 'an user trying to sign in' do
-    user = create(:user)
-    visit '/admin'
+  context 'sign in' do
+    scenario 'user' do
+      user = create(:user)
+      visit new_user_session_path
 
-    fill_in 'Email', :with => user.email
-    fill_in 'Password', :with => user.password
-    click_button 'Login'
+      fill_in 'Email', :with => user.email
+      fill_in 'Senha', :with => user.password
+      click_button 'Login'
 
-    expect(page).to have_content 'Autenticado com sucesso.'
+      expect(page).to have_content 'Autenticado com sucesso.'
+    end
   end
-
 end
