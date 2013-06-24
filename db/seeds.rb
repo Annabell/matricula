2e6009d84ab63ba46d99db18ab83164ca24dc80c
Douglas Camata <d.camata@gmail.com>
2013-06-23 22:27:13 -0300

    seed com disciplinas importadas do "rick rocks"

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Discipline.create! cod: 'MAT01204', name: 'Álgebra Linear - Engenharias', credits: 4
+Discipline.create! cod: 'MAT01105', name: 'Cálculo Integral e diferencial IIÍ', credits: 4
+Discipline.create! cod: 'INF01202', name: 'Estrutura de Dados II', credits: 3
+Discipline.create! cod: 'INF01114', name: 'Estrutura de Dados I', credits: 3
+Discipline.create! cod: 'MAT01208', name: 'Cálculo Numérico', credits: 4
+Discipline.create! cod: 'MAT01106', name: 'Método Matemático', credits: 4
+Discipline.create! cod: 'INF01104', name: 'Lógica Digital', credits: 3
+Discipline.create! cod: 'INF01105', name: 'Organização de Computadores', credits: 4
+Discipline.create! cod: 'INF01209', name: 'Programação II', credits: 3
+Discipline.create! cod: 'INF01106', name: 'Programação I', credits: 3
+Discipline.create! cod: 'INF01207', name: 'Estruturas Discretas', credits: 4
+Discipline.create! cod: 'MAT01104', name: 'Lógica Matemática', credits: 3
+Discipline.create! cod: 'MAT01203', name: 'Cálculo Integral e diferencial II', credits: 5
+Discipline.create! cod: 'MAT01212', name: 'Álgebra Linear - Computação', credits: 4
+Discipline.create! cod: 'MAT01101', name: 'Cálculo Integral e diferencial I', credits: 6
+Discipline.create! cod: 'MAT01117', name: 'Geometria Analítica e Vetores', credits: 4
