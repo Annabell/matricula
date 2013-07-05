8960742ff2d4b059830b03151151f32bcce80610
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-07-05 13:41:50 -0300

    Add other side of schedule association

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,6 +1,7 @@
 class Classroom < ActiveRecord::Base
   attr_accessible :name, :vacancies, :discipline_id
   belongs_to :discipline
+  has_many :schedules
 
   validates :vacancies, :name, presence: true
 end
