06111c16cff3542b7e66e399694e01d6959b98ba
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-07-07 00:30:47 -0300

    Adicionando validação dos horarios.

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,5 +3,13 @@
 class Schedule < ActiveRecord::Base
   belongs_to :classroom
   attr_accessible :day, :time_begin, :time_end
-  validates_presence_of :day, :time_begin, :time_end
+  validates :day, :time_begin, :time_end, presence: true
+  before_save :validate_times
+
+
+  def validate_times
+  	if self.time_begin > self.time_end
+  		errors.add(:time_end, "Horario de término tem que ser após horario de início.")
+  	end
+  end
 end
