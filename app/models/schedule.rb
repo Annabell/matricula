679fdfe76d3f03efe1065c2980fbac4c0b3bc790
Thawan Fidelis <thawan.fidelis@gmail.com>
2013-07-08 13:05:16 -0300

    Adicionando condição para nao quebrar testes de 1 atributo só

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -4,12 +4,14 @@ class Schedule < ActiveRecord::Base
   belongs_to :classroom
   attr_accessible :day, :time_begin, :time_end, :classroom_id
   validates :day, :time_begin, :time_end, :classroom_id, presence: true
-  before_save :validate_times
+  before_validation :validate_times
 
 
   def validate_times
-  	if self.time_begin > self.time_end
-  		errors.add(:time_end, "Horario de término tem que ser após horario de início.")
+  	unless(self.time_begin.nil? or self.time_end.nil?) #TODO retirar essa condição (quebra os testes que nao passam todos att)
+  		if self.time_begin > self.time_end
+  			errors.add(:time_end, "tem que ser após Horário Inicial.")
+  		end
   	end
   end
 end
